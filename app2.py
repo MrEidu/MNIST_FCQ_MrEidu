b6adc9fc3628f9eb5bc45d7e5f46946728dba662
Angel Eidu Macías Sáenz <47164087+MrEidu@users.noreply.github.com>
2022-10-20 09:21:39 -0600

    added files

---
+++ app2.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from img_classification2 import teachable_machine_classification
+import keras
+from PIL import Image, ImageOps
+import numpy as np
+
+uploaded_file = st.file_uploader("Carga una imagen ...", type=["jpg","jpeg","png"])
+if uploaded_file is not None:
+  image = Image.open(uploaded_file)
+  label = teachable_machine_classification(image, 'keras_model.h5') 
+  if label == 0:
+    texto = "Brick 1x"
+  if label == 1:
+    texto = "Brick Bow"
+  if label == 2:
+    texto = "Brick 2x"
+  if label == 3:
+    texto = "Flat Tile"
+  if label == 4:
+    texto = "Tile"
+  st.image(image, caption=texto)
+  #st.write(label)
