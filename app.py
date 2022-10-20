50fbd5b1500efb11a1bcf6f1bded98acbebb29d0
Angel Eidu Macías Sáenz <47164087+MrEidu@users.noreply.github.com>
2022-10-20 08:40:43 -0600

    Create app.py
    Created file App.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.title("Mi Aplicacion")
+st.sidebar.header("Esta Aplicacion es una demo")
+st.sidebar.image("https://mario.wiki.gallery/images/thumb/4/42/NSMBU_Green_Yoshi_Egg_Artwork.png/400px-NSMBU_Green_Yoshi_Egg_Artwork.png?download")
+boton = st.button("Launch a globo")
+if boton:
+  st.balloon()
+  st.balloon()
+  st.balloon()
